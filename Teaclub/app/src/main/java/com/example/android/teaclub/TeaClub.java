2b78a5b79f0637311e148c9bc39e2bd5b39984e3
Dominika <kumkwaczka@gmail.com>
2017-03-26 13:21:43 +0200

    adding projects

---
+++ Teaclub/app/src/main/java/com/example/android/teaclub/TeaClub.java
@@ -0,0 +1,13 @@
+package com.example.android.teaclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeaClub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content);
+    }
+}
